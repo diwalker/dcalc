e1427b0c0fea5f361353c105ee8069689c66ec2a
Diego Kravczyk <diwalker13@icloud.com>
2021-11-15 12:56:23 -0300

    App de uma calculadora simples em CSS e Javascript

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function insert (num) {
+  var numero = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = numero + num;
+}
+
+function clean() {
+  document.getElementById('resultado').innerHTML = "";
+}
+
+function back() {
+  var numero = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = numero.substring(0, numero.length - 1);
+}
+
+function calc() {
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = eval(numero);
+}
